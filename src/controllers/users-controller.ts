6369bbd33ad8eae8489d6699aa699683b3af3321
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-26 14:53:06 -0300

    first commit

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+class UsersController {
+  create = (request: Request, response: Response) => {
+    return response.json({ message: "ok" });
+  }
+}
+
+
+
+
+export { UsersController };
