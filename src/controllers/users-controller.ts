df0b4847b277a6671357114e683584e43e1251b8
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-27 19:35:44 -0300

    feat: add user registration and session creation functionality
    - Added bcrypt for password hashing.
    - Created SessionsController with a method to handle session creation.
    - Updated UsersController to handle user registration with validation and password hashing.
    - Introduced Prisma client for database interactions.
    - Added routes for user and session management.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,39 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import {z} from "zod";
+import { hash } from "bcrypt";
 
 class UsersController {
-  create = (request: Request, response: Response) => {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, "Name is required"),
+      email: z.string().email("Invalid email format"),
+      password: z.string().min(6, "Password must be at least 6 characters long")
+    })
+    
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const userWhithSameEmail = await prisma.user.findFirst({where: { email }});
+
+    if (userWhithSameEmail) {
+      throw new AppError("Email already in use", 400);
+    }
+
+    const hashedPassword = await hash(password, 8);
+    
+    const user = await prisma.user.create({
+      data:{
+        name,
+        email,
+        password: hashedPassword,
+      }
+    })
+    
+    const { password: _, ...userWhithoutPassword } = user
+
+    return response.status(201).json(userWhithoutPassword);
   }
 }
 
