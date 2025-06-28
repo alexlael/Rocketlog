da75e9930e59815e5d26fa4aabe630ef53ff4561
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-28 20:41:21 -0300

    feat: implement session creation with user authentication

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,10 +1,36 @@
-import { Request, Response, } from "express";
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
 
 class SessionsController {
-  create(request: Request, response: Response) {
-   return response.json({message: "Session created successfully"});
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new AppError("User or password is wrong", 404);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("User or password is wrong", 401);
+    }
+
+    return response.json({ message: "Session created successfully" });
   }
 }
-
 
 export { SessionsController };
