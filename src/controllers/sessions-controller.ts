73ce10f78fdc0120e9c9e2e763af4084fd61a016
Alex Lael <alex.lael.bsb@gmail.com>
2025-06-29 05:48:48 -0300

    feat: add JWT authentication support with environment configuration

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/database/prisma";
 import { z } from "zod";
 import { AppError } from "@/utils/AppError";
 import { compare } from "bcrypt";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
   async create(request: Request, response: Response) {
@@ -29,7 +31,15 @@ class SessionsController {
       throw new AppError("User or password is wrong", 401);
     }
 
-    return response.json({ message: "Session created successfully" });
+    const {secret, expirsIn} = authConfig.jwt;
+
+    const token = sign({role:user.role ?? "customer"}, secret, {
+      subject: user.id,
+      expiresIn: expirsIn,})
+
+      const {password:hashedPassword, ...userWithoutPassword} = user;
+
+    return response.json({ token, user: userWithoutPassword });
   }
 }
 
