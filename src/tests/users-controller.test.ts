d7416a4883bd4f83b679c83b0b349a77f003f669
Alex Lael <alex.lael.bsb@gmail.com>
2025-07-03 22:47:57 -0300

    feat: adiciona teste para criação de usuário no UsersController

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,5 +1,17 @@
+import  request  from "supertest"
+import { app } from "@/app"
+
 describe("UsersController",() => {
-    it("test", () => {
-        console.log("Passou por aqui")
+    it("should create a new user successfully", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Test User",
+            email: "teste@email.com",
+            password: "123456",
+        })
+
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty("id")
+        expect(response.body.name).toBe("Test User")
+        
     })
 })
